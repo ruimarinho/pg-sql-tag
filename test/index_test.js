9bff06fc904a597fd9954657880d42a1c754b119
Nuno Sousa <nuno@uphold.com>
2016-05-04 00:07:16 +0100

    Add rewritten ES2016 implementation

--- test/index_test.js
+++ test/index_test.js
@@ -1,48 +1,44 @@
+'use strict';
 
 /**
  * Test `pg-sql-tag`.
  */
 
-var pg = require('pg');
+import Promise from 'bluebird';
+import pg from 'pg';
+import sql from 'sql-tag';
+import tag from '../src';
 
-require('..')(pg);
-require('thenify-all')(pg.Client.prototype, pg.Client.prototype, ['connect', 'query']);
+// Promisify the pg client.
+Promise.promisifyAll(pg.Client.prototype);
 
-describe('pg-sql-tag', function() {
-  // psql -d postgres
-  // create role postgres with login
-  // createdb pg-sql-tag --owner postgres
-  var client = new pg.Client('postgres://postgres:@localhost/pg-sql-tag');
-  var db = client.connect()
-    .then(function() {
-      return client.query('DROP TABLE IF EXISTS foo');
-    })
-    .then(function() {
-      return client.query('CREATE TABLE foo ( bar char(1), biz char(1) )');
-    })
-    .then(function() {
-      return client.query('INSERT INTO foo VALUES (\'z\', \'x\')');
-    });
+// Install our monkey patch.
+tag(pg);
 
-  describe('normal sql queries', function() {
-    it('should continue supporting a sql `query` string', function() {
-      return db.then(function() {
-        return client.query('SELECT * FROM foo WHERE bar = \'z\' AND biz = \'x\'');
-      }).then(function(result) {
-        result.rows[0].bar.should.equal('z');
-        result.rows[0].biz.should.equal('x');
-      });
+describe('pg-sql-tag', () => {
+  const client = new pg.Client(`postgres://postgres:@postgres/pg-sql-tag`);
+  const db = client.connectAsync()
+    .then(() => client.queryAsync('DROP TABLE IF EXISTS foo'))
+    .then(() => client.queryAsync('CREATE TABLE foo ( bar char(1), biz char(1) )'))
+    .then(() => client.queryAsync(`INSERT INTO foo VALUES ('z', 'x')`));
+
+  describe('normal sql queries', () => {
+    it('should continue supporting a sql `query` string', () => {
+      return db.then(() => client.queryAsync(`SELECT * FROM foo WHERE bar = 'z' AND biz = 'x'`))
+        .then(result => {
+          result.rows[0].bar.should.equal('z');
+          result.rows[0].biz.should.equal('x');
+        });
     });
   });
 
-  describe('tagged sql queries', function() {
-    it('should supported tagged sql queries', function() {
-      return db.then(function() {
-        return client.query({ query: 'SELECT * FROM foo WHERE bar = ? AND biz = ?', values: ['z', 'x'] });
-      }).then(function(result) {
-        result.rows[0].bar.should.equal('z');
-        result.rows[0].biz.should.equal('x');
-      });
+  describe('tagged sql queries', () => {
+    it('should supported tagged sql queries', () => {
+      return db.then(() => client.queryAsync(sql`SELECT * FROM foo WHERE bar = ${'z'} AND biz = ${'x'}`))
+        .then(result => {
+          result.rows[0].bar.should.equal('z');
+          result.rows[0].biz.should.equal('x');
+        });
     });
   });
 });
